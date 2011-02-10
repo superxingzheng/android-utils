6a2052d6418c849e413081d2f65f1998a4833745
Ash Charles <ash@gumstix.com>
2011-02-09 16:12:04 -0800

    added a script to do a clean rebuild of the kernel, u-boot, and x-load

---
+++ compile.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+# rather fragile but...
+export PATH=${PWD}/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin:$PATH
+export ARCH=arm
+export CROSS_COMPILE=arm-eabi-
+export OUTPUTDIR=${PWD}/out/target/product/overo
+
+# Build u-boot
+cd uboot
+make distclean
+make omap3_overo_config
+make -j4
+cp u-boot.bin ${OUTPUTDIR}
+cd ..
+
+# Build kernel
+cd kernel
+make mrproper
+make overo_android_defconfig
+make -j4
+cp arch/arm/boot/uImage ${OUTPUTDIR}
+cd ..
+
+# Build MLO
+cd mlo
+make distclean
+make overo_config
+make -j4
+gcc scripts/signGP.c -o signGP
+./signGP x-load.bin 0x40200800
+cp x-load.bin.ift ${OUTPUTDIR}/MLO
+cd ..
+
