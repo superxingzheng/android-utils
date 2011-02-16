deda04ba4db1f4fddd88d50726543766adfd6a67
Ash Charles <ash@gumstix.com>
2011-02-16 14:11:45 -0800

    modified load-rootfs.sh to just build a rootfs tarball rather than unpacking one onto a disc

--- load-rootfs.sh
+++ load-rootfs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-MNTROOTFS=/media/rootfs
+#MNTROOTFS=/media/rootfs
 TARDIR=${PWD}/out/target/product/overo/
 
-echo "Clean old rootfs on SD"
-sudo rm -rf ${MNTROOTFS}/*
+#echo "Clean old rootfs on SD"
+#sudo rm -rf ${MNTROOTFS}/*
 cd ${TARDIR}
 echo "remove dir"
 rm -rf android_rootfs
@@ -16,11 +16,11 @@ echo "move system"
 cp -r system android_rootfs
 cd ${TARDIR}
 echo "make tarball"
-sudo ../../../../build/tools/mktarball.sh ../../../host/linux-x86/bin/fs_get_stats android_rootfs . rootfs rootfs.tar.bz2
-echo "untar"
-sudo tar xaf rootfs.tar.bz2 --numeric-owner -C ${MNTROOTFS}
-echo "sync"
-sync
-sync
+../../../../build/tools/mktarball.sh ../../../host/linux-x86/bin/fs_get_stats android_rootfs . rootfs rootfs.tar.bz2
+#echo "untar"
+#sudo tar xaf rootfs.tar.bz2 --numeric-owner -C ${MNTROOTFS}
+#echo "sync"
+#sync
+#sync
 cd ${PWD}
 echo "DONE!"
