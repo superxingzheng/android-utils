3268ffa57527c3b163d5b1b00d8667881d2bf88d
Ash Charles <ash@gumstix.com>
2011-02-18 17:07:09 -0800

    remove copy to card function from getter.sh as this is handled by mkandroidsd.sh

--- getter.sh
+++ getter.sh
@@ -2,17 +2,12 @@
 SERVER="cumulus.gumstix.org/Android/"
 SOURCE=$SERVER$1
 
+if [[ -z $1 ]];then
+    echo "Usage: $0 <build name>"
+fi
+
 wget $SOURCE/MLO
 wget $SOURCE/u-boot.bin
 wget $SOURCE/uImage
 wget $SOURCE/boot.scr
 wget $SOURCE/rootfs.tar.bz2
-
-cp MLO /media/boot
-cp u-boot.bin /media/boot
-cp uImage /media/boot
-cp boot.scr /media/boot
-fakeroot rm -rf /media/rootfs
-fakeroot tar xaf rootfs.tar.bz2 -C /media/rootfs/
-
-
