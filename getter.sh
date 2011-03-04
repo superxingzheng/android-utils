d2f77f60e863947f8611342cd401cb206beb4272
Ash Charles <ash@gumstix.com>
2011-03-04 10:56:37 -0800

    Fixed up getter script

--- getter.sh
+++ getter.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-SERVER="cumulus.gumstix.org/Android/"
-SOURCE=$SERVER$1
+SERVER="http://cumulus.gumstix.org/images/android"
+SOURCE="${SERVER}/current"
 
-if [[ -z $1 ]];then
-    echo "Usage: $0 <build name>"
+# We need curl.  Use curl as it allows for restarting downloads.
+command -v curl > /dev/null
+if [[ ! ($?) ]]; then
+   echo "'curl' utility not found. On Ubuntu, install with"
+   echo "    sudo apt-get install curl"
 fi
 
-wget $SOURCE/MLO
-wget $SOURCE/u-boot.bin
-wget $SOURCE/uImage
-wget $SOURCE/boot.scr
-wget $SOURCE/rootfs.tar.bz2
+# If no argument given, grab latest build.
+if [[ -z $1 ]]; then
+    echo "[ Downloading latest images from ${SERVER} ]"
+else
+    SOURCE=${SERVER}/$1
+    echo "[ Downloading images from ${SOURCE} ]"
+fi
+
+curl -O -# -C - ${SOURCE}/MLO
+curl -O -# -C - ${SOURCE}/u-boot.bin
+curl -O -# -C - ${SOURCE}/uImage
+curl -O -# -C - ${SOURCE}/boot.scr
+curl -O -# -C - ${SOURCE}/rootfs.tar.bz2
+curl -O -# -C - ${SOURCE}/md5sums.txt
+
+echo "[ Verifying downloads ]"
+md5sum -c md5sums.txt
