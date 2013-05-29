e7c77b9372d3206f5412cf186ecbe03d25ade7c3
Adam Lee <adam@gumstix.com>
2013-05-29 23:05:01 +0000

    Modified boot script to reflect partition scheme change

--- androidboot.cmd
+++ androidboot.cmd
@@ -4,10 +4,12 @@ if test ${bootdelay} -ne 1; then
   saveenv
 fi
 
-setenv anddisplay 'vram=12M omapfb.mode=dvi:1024x768MR-16@60 omapdss.def_disp=lcd43'
-setenv andconsole 'console=ttyS2,115200n8 console=tty0 androidboot.console=ttyS2'
-setenv androot 'root=/dev/mmcblk0p2 rw rootfstype=ext3 rootwait init=/init'
+setenv anddisplay 'vram=12M omapfb.mode=dvi:1024x768MR-16@60 omapdss.def_disp=dvi'
+setenv andconsole 'console=ttyO2,115200n8 androidboot.console=ttyO2'
+setenv androot 'root=/dev/ram0 rootwait init=/init'
 setenv andbootargs 'setenv bootargs mpurate=600 ${andconsole} ${androot} ${anddisplay}'
-
-setenv bootcmd 'mmc init; fatload mmc 0 ${loadaddr} uImage; run andbootargs; bootm ${loadaddr}'
+setenv loadaddr 0x82000000
+setenv loadaddr_ramdisk 0x81600000
+setenv mmcdev 0
+setenv bootcmd 'mmc rescan ${mmcdev}; fatload mmc 0 ${loadaddr} uImage; fatload mmc 0 ${loadaddr_ramdisk} uInitrd; run andbootargs; bootm ${loadaddr} ${loadaddr_ramdisk}'
 boot
